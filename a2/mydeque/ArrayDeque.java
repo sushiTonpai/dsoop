cd3284f3cc69acc425b3ae25294a0fc43e5a89df
Tonpai <paigood999@gmail.com>
2022-05-17 18:39:29 +0700

    gu hue kao

--- a2/mydeque/ArrayDeque.java
+++ a2/mydeque/ArrayDeque.java
@@ -1,20 +1,84 @@
 package a2.mydeque;
 
 public class ArrayDeque<T> {
-    private T[] item;
-    private int size;
+    private T[] items;
 
-//    constructor
-    public ArrayDeque(){
-        item = (T[]) new Object[8];
-        size = 0;
+    //    pointer
+    private int end;
+    private int start;
+    private int circSize;
+
+    //    constructor
+    public ArrayDeque() {
+        circSize = 8;
+        items = (T[]) new Object[circSize];
+        end = 0;
+        start = 0;
     }
 //    deep copy
+    public ArrayDeque(ArrayDeque<T> other){
+
+    }
 
 
+    private void grow(int newCap) {
+        T[] expand = (T[]) new Object[newCap];
+        System.arraycopy(items, 0, expand, 0, circSize);
+        items = expand;
+    }
+
+
+    public int size(){
+        return end-start;
+    }
+    public void addLast(T item) {
+        items[end] = item;
+        end++;
+    }
+
+    public void addFirst(T item) {
+        items[start] = item;
+        start++;
+    }
+
+    public T removeLast() {
+        return null;
+
+    }
+
+    public T removeFirst(){
+        return null;
+    }
+    public boolean isEmpty() {
+    return (end - start == 0);
+    }
+
+    public String toString(){
+        String out = "";
+        for (int pointer = start;pointer < end-1; pointer++){
+            out = out + (items[pointer]) + ", ";
+        }
+    return out + items[end-1];
+    }
+
+    public T get(int index){
+        if(index < 0 || index > end-start)
+            return null;
+        T out =  items[0];
+        for (int pointer = 0; pointer <= index;pointer++){
+            out = items[pointer];
+        }
+        return out;
+    }
 
 
     public static void main(String[] args) {
-
+        ArrayDeque<Integer>  ad = new ArrayDeque<>();
+        System.out.println(ad.isEmpty());
+        ad.addLast(2);
+        ad.addLast(3);
+        System.out.println(ad.isEmpty());
+//        System.out.println(ad.get(0));
+        System.out.println(ad);
     }
 }
